1fa0819979c47a4bbdadfb0aac6a41343cd2987a
ellaedmonds <35499735+ellaedmonds@users.noreply.github.com>
2018-10-09 12:06:01 -0400

    Updated from Brython Server: 10/9/2018 12:05:55 PM

--- cryptography.py
+++ cryptography.py
@@ -14,27 +14,51 @@ a = input('Enter e to encrypt, d to decrypt, or q to quit: ')
 if a != "q" and a != "e" and a != "d":
     print("Did not understand command, try again.")
 
-if a == "q":
+elif a == "q":
     print("Goodbye!")
     
-if a == "e" or "d":
+elif a == "e" or "d":
     b = input ("Message: ")
     c = input ("Key: ")
- 
-associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
-
-message= []
-for n in b:
-    d = associations.find(n)
-    message.append(d)
-
-key = []
-for n in c:
-    e = associations.find(n)
-    key.append(e)
     
-print(message)
-print(key)
+    associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+
+    message= []
+    for n in b:
+     d = associations.find(n)
+     message.append(d)
+
+    key1 = []
+    for n in c:
+        e = associations.find(n)
+        key1.append(e)
+    
+    print(message)
+    print(key1)
+
+    M = len(message)
+    K = len(key1)
+
+    key = key1*(M/K)
+
+    print(key)
+
+    crypt1 = zip(key,message)
+    crypt = []
+    for c in crypt1:
+        crypt.append(c[0]+c[1])
+
+    print(crypt)
+    
+    final = []
+    for c in crypt:
+        C = associations[c]
+        final.append(C)
+    
+    print(final)
+    
+    for n in final:
+        print(n)
 
 
 
